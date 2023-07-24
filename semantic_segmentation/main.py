17ae22b9d60d06100c5b77f9f970f441dca56bbc
Kaixin Bai <kb@ar-mps01.ar.int>
2023-07-24 16:00:50 +0200

    update commit

--- semantic_segmentation/main.py
+++ semantic_segmentation/main.py
@@ -381,7 +381,7 @@ def validate(segmenter, input_types, val_loader, epoch, num_classes=-1, save_ima
             gt_idx = gt < num_classes  # Ignore every class index larger than the number of classes
             # Compute outputs
             # outputs, alpha_soft = segmenter(inputs)
-            outputs, _ = segmenter(inputs)  # batch个 4个semantic分割图，有40类。TODO：为啥是4个图？
+            outputs, _ = segmenter(inputs)  # batch个 4个semantic分割图，有40类
             for idx, output in enumerate(outputs):
                 output = cv2.resize(output[0, :num_classes].data.cpu().numpy().transpose(1, 2, 0),
                                     target.size()[1:][::-1],
