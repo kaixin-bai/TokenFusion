448e0fb9e100decf683f74bc0a006cb0d2a5cde9
Kaixin Bai <kaixin.bai@ar-a100-gpu.agile-robots.de>
2023-10-18 08:08:58 +0000

    update multimodal decoder modify

--- semantic_segmentation/models/segformer.py
+++ semantic_segmentation/models/segformer.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 from . import mix_transformer
 from mmcv.cnn import ConvModule
 from .modules import num_parallel
@@ -10,6 +10,7 @@ class MLP(nn.Module):
     """
     Linear Embedding
     """
+
     def __init__(self, input_dim=2048, embed_dim=768):
         super().__init__()
         self.proj = nn.Linear(input_dim, embed_dim)
@@ -24,6 +25,7 @@ class SegFormerHead(nn.Module):
     """
     SegFormer: Simple and Efficient Design for Semantic Segmentation with Transformers
     """
+
     def __init__(self, feature_strides=None, in_channels=128, embedding_dim=256, num_classes=20, **kwargs):
         super(SegFormerHead, self).__init__()
         self.in_channels = in_channels
@@ -34,8 +36,8 @@ class SegFormerHead(nn.Module):
 
         c1_in_channels, c2_in_channels, c3_in_channels, c4_in_channels = self.in_channels
 
-        #decoder_params = kwargs['decoder_params']
-        #embedding_dim = decoder_params['embed_dim']
+        # decoder_params = kwargs['decoder_params']
+        # embedding_dim = decoder_params['embed_dim']
 
         self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
         self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
@@ -44,7 +46,7 @@ class SegFormerHead(nn.Module):
         self.dropout = nn.Dropout2d(0.1)
 
         self.linear_fuse = ConvModule(
-            in_channels=embedding_dim*4,
+            in_channels=embedding_dim * 4,
             out_channels=embedding_dim,
             kernel_size=1,
             norm_cfg=dict(type='BN', requires_grad=True)
@@ -58,16 +60,16 @@ class SegFormerHead(nn.Module):
         ############## MLP decoder on C1-C4 ###########
         n, _, h, w = c4.shape
 
-        _c4 = self.linear_c4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
-        _c4 = F.interpolate(_c4, size=c1.size()[2:],mode='bilinear',align_corners=False)
+        _c4 = self.linear_c4(c4).permute(0, 2, 1).reshape(n, -1, c4.shape[2], c4.shape[3])
+        _c4 = F.interpolate(_c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
 
-        _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
-        _c3 = F.interpolate(_c3, size=c1.size()[2:],mode='bilinear',align_corners=False)
+        _c3 = self.linear_c3(c3).permute(0, 2, 1).reshape(n, -1, c3.shape[2], c3.shape[3])
+        _c3 = F.interpolate(_c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
 
-        _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
-        _c2 = F.interpolate(_c2, size=c1.size()[2:],mode='bilinear',align_corners=False)
+        _c2 = self.linear_c2(c2).permute(0, 2, 1).reshape(n, -1, c2.shape[2], c2.shape[3])
+        _c2 = F.interpolate(_c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
 
-        _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
+        _c1 = self.linear_c1(c1).permute(0, 2, 1).reshape(n, -1, c1.shape[2], c1.shape[3])
 
         _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
 
@@ -77,18 +79,154 @@ class SegFormerHead(nn.Module):
         return x
 
 
+class ModifiedSegFormerHead(nn.Module):
+    def __init__(self, feature_strides=None, in_channels=128, embedding_dim=256, num_classes=20, **kwargs):
+        super(ModifiedSegFormerHead, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        assert len(feature_strides) == len(self.in_channels)
+        assert min(feature_strides) == feature_strides[0]
+        self.feature_strides = feature_strides
+
+        c1_in_channels, c2_in_channels, c3_in_channels, c4_in_channels = self.in_channels
+
+        # decoder_params = kwargs['decoder_params']
+        # embedding_dim = decoder_params['embed_dim']
+
+        self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
+        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
+        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
+        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
+        self.dropout = nn.Dropout2d(0.1)
+
+        self.linear_fuse = ConvModule(
+            in_channels=embedding_dim * 4,
+            out_channels=embedding_dim,
+            kernel_size=1,
+            norm_cfg=dict(type='BN', requires_grad=True)
+        )
+
+        self.ca = ChannelAttention(in_channels=256)
+
+        self.linear_pred = nn.Conv2d(embedding_dim, self.num_classes, kernel_size=1)
+
+    def forward(self, s_features, m_features):
+        def __transform(x):
+            c1, c2, c3, c4 = x
+            ############## MLP decoder on C1-C4 ###########
+            n, _, h, w = c4.shape
+            _c4 = self.linear_c4(c4).permute(0, 2, 1).reshape(n, -1, c4.shape[2], c4.shape[3])
+            _c4 = F.interpolate(_c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
+            _c3 = self.linear_c3(c3).permute(0, 2, 1).reshape(n, -1, c3.shape[2], c3.shape[3])
+            _c3 = F.interpolate(_c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
+            _c2 = self.linear_c2(c2).permute(0, 2, 1).reshape(n, -1, c2.shape[2], c2.shape[3])
+            _c2 = F.interpolate(_c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
+            _c1 = self.linear_c1(c1).permute(0, 2, 1).reshape(n, -1, c1.shape[2], c1.shape[3])
+            _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
+            x = self.dropout(_c)
+            return x
+
+        x_s = __transform(s_features)
+        x_m = __transform(m_features)
+
+        # Channel attention mechanism
+        attention_applied = self.ca(x_s, x_m)
+        # Fusion of features with attention and residual connection
+        fused_features = attention_applied + x_m  # reinforcing the m_features
+        # Prediction layer
+        x = self.linear_pred(fused_features)
+        # x = self.linear_pred(x_s)
+        return x
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, s, m):
+        # Using s_features to calculate the attention weights
+        avg_out = self.fc(self.avg_pool(s))
+        max_out = self.fc(self.max_pool(s))
+        out = avg_out + max_out
+        weights = self.sigmoid(out)
+        # Apply weights to m_features
+        return weights * m  # 用single计算通道注意力权重，确保m_features是主导
+
+
+# class WeTr(nn.Module):
+#     def __init__(self, backbone:str, num_classes=20, embedding_dim=256, pretrained=True):
+#         super().__init__()
+#         self.num_classes = num_classes  # 40
+#         self.embedding_dim = embedding_dim  # 256
+#         self.feature_strides = [4, 8, 16, 32]
+#         self.num_parallel = num_parallel  # 2
+#         #self.in_channels = [32, 64, 160, 256]
+#         #self.in_channels = [64, 128, 320, 512]
+#
+#         # 先定义encoder的结构
+#         self.encoder = getattr(mix_transformer, backbone)()  # getattr用于获取对象的属性或方法，根据backbone的str来读取mix_transformer中叫做backbone的类的属性
+#         self.in_channels = self.encoder.embed_dims  # [64, 128, 320, 512]
+#         ## initilize encoder
+#         # 根据定义的encoder的结构来读取对应的segformer的pretrain model
+#         if pretrained:
+#             state_dict = torch.load('pretrained/' + backbone + '.pth')
+#             state_dict.pop('head.weight')
+#             state_dict.pop('head.bias')
+#             state_dict = expand_state_dict(self.encoder.state_dict(), state_dict, self.num_parallel)
+#             self.encoder.load_state_dict(state_dict, strict=True)
+#
+#         self.decoder = SegFormerHead(feature_strides=self.feature_strides, in_channels=self.in_channels,
+#                                      embedding_dim=self.embedding_dim, num_classes=self.num_classes)
+#
+#         self.alpha = nn.Parameter(torch.ones(self.num_parallel, requires_grad=True))
+#         self.register_parameter('alpha', self.alpha)
+#
+#     def get_param_groups(self):
+#         param_groups = [[], [], []]
+#         for name, param in list(self.encoder.named_parameters()):
+#             if "norm" in name:
+#                 param_groups[1].append(param)
+#             else:
+#                 param_groups[0].append(param)
+#         for param in list(self.decoder.parameters()):
+#             param_groups[2].append(param)
+#         return param_groups
+#
+#     def forward(self, x):
+#         """
+#         这个类其实就是一个segformer，不同的是它把encoder和decoder分开写，然后把encoder里每个block中的经过softmax的score保存在masks中，
+#         然后decoder依旧是传入x，但是masks就直接传出。
+#         """
+#         x, masks = self.encoder(x)  # 输入的x: {list:2} 每个元组都是一个Tensor [1,3,486,625]，分别是rgb和depth，depth是三通道，直接扩充的
+#         x = [self.decoder(x[0]), self.decoder(x[1])] # x为{list:2}, 每个元素是一个{list:4}，其中是4个Tensor，shape分别是[1,64,117,157],[1,128,59,79],[1,320,30,40],[1,512,15,20]
+#         ens = 0
+#         alpha_soft = F.softmax(self.alpha)
+#         for l in range(self.num_parallel):
+#             ens += alpha_soft[l] * x[l].detach()
+#         x.append(ens)  # 输入ens:[1,40,117,157]；输出x:{list3},每个Tensor[1,40,117,157]
+#         return x, masks  # masks是个{list:8}，每个又是个{list:2}，然后这8个里面的shape是[1,18369],[1,18369],[1,4661],[1,4661],[1,1200],[1,1200],[1,300],[1,300]
+
 class WeTr(nn.Module):
-    def __init__(self, backbone:str, num_classes=20, embedding_dim=256, pretrained=True):
+    def __init__(self, backbone: str, num_classes=20, embedding_dim=256, pretrained=True):
         super().__init__()
         self.num_classes = num_classes  # 40
         self.embedding_dim = embedding_dim  # 256
         self.feature_strides = [4, 8, 16, 32]
         self.num_parallel = num_parallel  # 2
-        #self.in_channels = [32, 64, 160, 256]
-        #self.in_channels = [64, 128, 320, 512]
+        # self.in_channels = [32, 64, 160, 256]
+        # self.in_channels = [64, 128, 320, 512]
 
         # 先定义encoder的结构
-        self.encoder = getattr(mix_transformer, backbone)()  # getattr用于获取对象的属性或方法，根据backbone的str来读取mix_transformer中叫做backbone的类的属性
+        self.encoder = getattr(mix_transformer,
+                               backbone)()  # getattr用于获取对象的属性或方法，根据backbone的str来读取mix_transformer中叫做backbone的类的属性
         self.in_channels = self.encoder.embed_dims  # [64, 128, 320, 512]
         ## initilize encoder
         # 根据定义的encoder的结构来读取对应的segformer的pretrain model
@@ -98,9 +236,9 @@ class WeTr(nn.Module):
             state_dict.pop('head.bias')
             state_dict = expand_state_dict(self.encoder.state_dict(), state_dict, self.num_parallel)
             self.encoder.load_state_dict(state_dict, strict=True)
-
-        self.decoder = SegFormerHead(feature_strides=self.feature_strides, in_channels=self.in_channels, 
-                                     embedding_dim=self.embedding_dim, num_classes=self.num_classes)
+        print('debug self.in_channels: ', self.in_channels)
+        self.decoder = ModifiedSegFormerHead(feature_strides=self.feature_strides, in_channels=self.in_channels,
+                                             embedding_dim=self.embedding_dim, num_classes=self.num_classes)
 
         self.alpha = nn.Parameter(torch.ones(self.num_parallel, requires_grad=True))
         self.register_parameter('alpha', self.alpha)
@@ -122,7 +260,8 @@ class WeTr(nn.Module):
         然后decoder依旧是传入x，但是masks就直接传出。
         """
         x, masks = self.encoder(x)  # 输入的x: {list:2} 每个元组都是一个Tensor [1,3,486,625]，分别是rgb和depth，depth是三通道，直接扩充的
-        x = [self.decoder(x[0]), self.decoder(x[1])] # x为{list:2}, 每个元素是一个{list:4}，其中是4个Tensor，shape分别是[1,64,117,157],[1,128,59,79],[1,320,30,40],[1,512,15,20]
+        # x = [self.decoder(x[0]), self.decoder(x[1])] # x为{list:2}, 每个元素是一个{list:4}，其中是4个Tensor，shape分别是[1,64,117,157],[1,128,59,79],[1,320,30,40],[1,512,15,20]
+        x = [self.decoder(s_features=x[2], m_features=x[0]), self.decoder(s_features=x[2], m_features=x[1])]
         ens = 0
         alpha_soft = F.softmax(self.alpha)
         for l in range(self.num_parallel):
@@ -145,13 +284,13 @@ def expand_state_dict(model_dict, state_dict, num_parallel):
             if replace and model_dict_key_re in state_dict_keys:
                 model_dict[model_dict_key] = state_dict[model_dict_key_re]
     return model_dict
-    
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # import torch.distributed as dist
     # dist.init_process_group('gloo', init_method='file:///temp/somefile', rank=0, world_size=1)
     pretrained_weights = torch.load('pretrained/mit_b1.pth')
     wetr = WeTr('mit_b1', num_classes=20, embedding_dim=256, pretrained=True).cuda()
     wetr.get_param_groupsv()
-    dummy_input = torch.rand(2,3,512,512).cuda()
+    dummy_input = torch.rand(2, 3, 512, 512).cuda()
     wetr(dummy_input)
